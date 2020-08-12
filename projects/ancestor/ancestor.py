89cc541f3777a2efee27d9928633b02c8dda7f2b
aldairbalanzar <abalanzar7@gmail.com>
2020-08-12 16:32:30 -0400

    added notes to ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -3,36 +3,55 @@ from queue import Queue
 from util import Stack
 
 def earliest_ancestor(ancestors, start):
-    graph = Graph()
-    for parent, child in ancestors:
+    graph = Graph()                         # init graph
+    for parent, child in ancestors:         # populate it with vertices (ancestors) and add edges (connections)
         graph.add_vertex(parent)
         graph.add_vertex(child)
         graph.add_edge(child, parent)
 
-    q = Queue()
-    q.enqueue([start])
-    longest_path = 1
+    q = Queue()                             # start our queue
+    q.enqueue([start])                      # enqueue a path with the starting vertex
+    longest_path = 1                        # longest path defaults to 1 because we are always given a vertex to start from
     earliest_ancestor = -1
 
-    while q.size() > 0:
-        path = q.dequeue()
-        current_node = path[-1]
-
-        if len(path) >= longest_path and current_node < earliest_ancestor:
-            if current_node < earliest_ancestor:
+    while q.size() > 0:                     # while our queue size is greater than 0...
+        path = q.dequeue()                  # assign whatever we dequeue to our path variable
+        current_node = path[-1]             #current_node will be whatever is at the end of our path array
+        """
+        first IF:
+            if the length of our path is greater than or equal to what longest_path currently is
+            AND our current_node is less than what our earliest_ancestor currentyl is, then
+            earliest_ancestor will be whatever our current_node is
+        
+        second IF:
+            if the length of our path is greater than whatever our longest_path currently is, then
+            longest_path will be the length of path and earliest_ancestor will be current_node
+        """
+        if len(path) >= longest_path and current_node < earliest_ancestor:  
                 earliest_ancestor = current_node
         if len(path) > longest_path:
             longest_path = len(path)
             earliest_ancestor = current_node
         
+        neighbors = graph.get_neighbors(current_node)   # get the neighbors from the current node
+        # print(neighbors)
+        for ancestor in neighbors:                      # for each ancestor we get from neighbors
+            # print(f'ancestor: {ancestor}')
+            path_copy = path[:]                         # create a path_copy and append that ancestor to path_copy
+            path_copy.append(ancestor)    
+            print(f'COPY: {path_copy}')              
+            q.enqueue(path_copy)                        # enqueue path_copy similar to how we did with our start
+            print(f'SIZE: {q.size()}')
+        
+        """
+        our loop will end whenever we enqueue something with
+        the length of 0, otherwise this for loop is basically
+        the engine to our while loop
+        """
+    if q.size() <= 0:
+        print('size has reached zero')
 
-        neighbors = graph.get_neighbors(current_node)
-        print(neighbors)
-        for ancestor in neighbors:
-            path_copy = path[:]
-            path_copy.append(ancestor)
-            q.enqueue(path_copy)
-
+    # return whatever our earliest_ancestor ends up being
     return earliest_ancestor
 
 
